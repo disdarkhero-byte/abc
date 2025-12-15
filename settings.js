e31332f92b6bc3955fcc04f022b61036c94540bd
disdarkhero-byte <dis.darkhero@gmail.com>
2025-12-15 23:33:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=131618a96aea6c0a
+SESSION_ID: process.env.SESSION_ID === undefined ? '
 ' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
